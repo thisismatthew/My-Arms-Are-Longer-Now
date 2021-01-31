e707707dc00974d6025572d3b8f9d8ed50979254
Matthew <102381833@student.swin.edu.au>
2021-01-31 14:08:47 +1100

    + just pushing before cleaning up unnesesary assets

---
+++ GGJ2021/Assets/Scripts/Timer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    public float time = 10f;
+    public string nextSceneTitle;
+
+    // Update is called once per frame
+    void Update()
+    {
+        time -= Time.deltaTime;
+        if (time < 0)
+        {
+            SceneManager.LoadScene(nextSceneTitle);
+        }
+        
+    }
+}
